62dd0bbb01e8014429de069a3db66eb64947fbb1
nickty <nickty.86@gmail.com>
2021-07-02 22:31:59 +0600

    user logout

--- src/App.js
+++ src/App.js
@@ -7,7 +7,35 @@ import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
 import RegisterComplete from './pages/auth/RegisterComplete';
 
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react';
+import { auth } from './firebase';
+
+
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if(user){
+        const idTokenResult = await user.getIdTokenResult()
+
+        dispatch({
+          type:'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token
+          }
+        })
+      }
+    }) 
+
+    return () => unsubscribe(); 
+  }, [])
+
+
   return (
 
   <>
--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { Menu } from 'antd'
-import { UserAddOutlined, AppstoreOutlined, SettingFilled, LoginOutlined } from '@ant-design/icons'
+import { UserAddOutlined, AppstoreOutlined, SettingFilled, LoginOutlined, UserOutlined, LogoutOutlined } from '@ant-design/icons'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import firebase from 'firebase'
+import { useDispatch } from 'react-redux' 
 
 const { SubMenu, Item } = Menu
 
 const Header = () => {
 
     const [current, setCurrent ] = useState('home')
+    let dispatch = useDispatch();
+    const history = useHistory();
 
     const handleClick = (e) => {
         setCurrent(e.key)
+    }
+
+    const logout = () => {
+        firebase.auth().signOut();
+
+        
+
+        dispatch({
+            type: "LOGOUT", 
+            payload: null
+        })
+
+        history.push('/login')
     }
 
     return (
@@ -26,6 +43,7 @@ const Header = () => {
                 
                     <Item key="Setting:1">Option 1</Item>
                     <Item key="Setting:2">Option 2</Item>
+                    <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
     
             </SubMenu>
 
