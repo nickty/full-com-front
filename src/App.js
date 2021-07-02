8a9e38b59bf603cd4eec3918813a04290349ab32
nickty <nickty.86@gmail.com>
2021-07-02 11:55:34 +0600

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { Switch, Route} from 'react-router-dom'
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      react app
-    </div>
+    
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    
   );
 }
 
