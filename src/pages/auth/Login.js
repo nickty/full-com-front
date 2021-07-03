9d56fc0ec81d9bea554877e014d13467785c905b
nickty <nickty.86@gmail.com>
2021-07-03 09:00:01 +0600

    login form created

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import { auth } from '../../firebase';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+       console.log(email)
+
+    }
+    const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+        <input placeholder="Enter your email" type="email" className="form-control" name="" value={email} onChange={e => setEmail(e.target.value)} autoFocus />
+        <input placeholder="Enter your password" type="password" className="form-control mt-3" name="" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit" className="btn btn-info mt-3 w-100" disabled={ !email || password.length < 6 }>Login</button>
+    </form>
+    )
     return (
-        <div>
-            Login
+        <div className="container p-5">
+           <div className="row">
+               <div className="col-md-6 offset-md-3">
+                   <h4>Login</h4>
+                   
+                   {loginForm()}
+               </div>
+           </div>
         </div>
     )
 }
