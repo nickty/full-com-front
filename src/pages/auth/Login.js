7fc70f65b20c0d0baf0683948816f518a1d6d28d
nickty <nickty.86@gmail.com>
2021-07-03 09:33:56 +0600

    google login with user working

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,8 @@
+import { LoadingOutlined, LoginOutlined } from '@ant-design/icons';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-import { auth } from '../../firebase';
+import { auth, googleAuthProvider } from '../../firebase';
 
 const Login = ({ history }) => {
 
@@ -41,11 +42,34 @@ const Login = ({ history }) => {
        }
 
     }
+
+    const googleLogin = async () => {
+       await auth.signInWithPopup(googleAuthProvider)
+       .then( result => {
+           const { user } = result
+           const idTokenResult = user.getIdTokenResult();
+
+           dispatch({
+            type:'LOGGED_IN_USER',
+            payload: {
+            email: user.email,
+            token: idTokenResult.token
+            }
+        })
+
+            history.push('/')
+       })
+       .catch(err => {
+        console.log(err)
+        toast.error(err.message)
+       })
+    }
     const loginForm = () => (
     <form onSubmit={handleSubmit}>
         <input placeholder="Enter your email" type="email" className="form-control" name="" value={email} onChange={e => setEmail(e.target.value)} autoFocus />
         <input placeholder="Enter your password" type="password" className="form-control mt-3" name="" value={password} onChange={e => setPassword(e.target.value)} />
-        <button type="submit" className="btn btn-info mt-3 w-100" disabled={ !email || password.length < 6 }>Login</button>
+        <button type="submit" className="btn btn-info mt-3 w-100" disabled={ !email || password.length < 6 }>Login with Email and Password</button>
+        
     </form>
     )
     return (
@@ -53,8 +77,9 @@ const Login = ({ history }) => {
            <div className="row">
                <div className="col-md-6 offset-md-3">
                    <h4>Login</h4>
-                   
+                   {loading ? 'Loading..' : ''}
                    {loginForm()}
+                   <button onClick={googleLogin} className="btn btn-primary mt-3 w-100">Login with Google</button>
                </div>
            </div>
         </div>
