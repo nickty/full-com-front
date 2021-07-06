8590f462badea6d345aeb52d33d12b3075441601
nickty <nickty.86@gmail.com>
2021-07-06 21:53:28 +0600

    product loaded into dashboard

--- src/functions/product.js
+++ src/functions/product.js
@@ -6,3 +6,7 @@ export const createProduct = async (product, authtoken) =>{
         }
     })
 }
+
+export const getProductsByCount = async (count) =>{
+    return await axios.get(`${process.env.REACT_APP_API}/products/${count}`)
+}
