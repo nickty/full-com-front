9649cea9c17868b905806723e7694adfa501fff2
nickty <nickty.86@gmail.com>
2021-07-10 10:42:50 +0600

    showing product in the home page first step

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProductsByCount } from '../functions/product'
 
 const Home = () => {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        loadingAllProducts()
+    }, [])
+
+    const loadingAllProducts = () => {
+        getProductsByCount(3).then((res) => {
+            setProducts(res.data)
+        })
+    }
     return (
         <div>
-            home
+            {JSON.stringify(products)}
         </div>
     )
 }
