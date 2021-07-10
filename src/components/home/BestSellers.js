4d75e211bf1ab007009f1a16b7b6b011e5848b77
nickty <nickty.86@gmail.com>
2021-07-10 21:32:50 +0600

    showing best sellers with pagination

--- src/components/home/BestSellers.js
+++ src/components/home/BestSellers.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import LoadingCard from '../cards/LoadingCard'
 import ProductCard from '../cards/ProductCard'
-import { getProducts } from '../../functions/product'
+import { getProducts, getProductsCount } from '../../functions/product'
+import { Pagination } from 'antd'
 
 const BestSellers = () => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(false)
+    const [page, setPage] = useState(1)
+    const [productsCount, setProductsCount] = useState(0)
 
     useEffect(() => {
         loadingAllProducts()
+    }, [page])
+
+    useEffect(() => {
+        getProductsCount().then(res => setProductsCount(res.data))
     }, [])
 
     const loadingAllProducts = () => {
         setLoading(true)
-        getProducts('sold', 'desc', 3).then((res) => {
+        getProducts('sold', 'desc', page).then((res) => {
             setProducts(res.data)
             setLoading(false)
         })
@@ -31,6 +38,13 @@ const BestSellers = () => {
                         ))}
                     </div>
                 )}
+                
+             </div>
+             <div className="row">
+                <nav className="col-md-4 offset-md-4 text-center pt-5 p-3">
+                <Pagination current={page} total={(productsCount / 3) * 10} onChange={value => setPage(value)} />
+                </nav>
+                
              </div>
             
         </div>
