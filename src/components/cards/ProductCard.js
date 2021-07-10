abd28a93ebb266d1d19b8809a3c5e5f3b97fd543
nickty <nickty.86@gmail.com>
2021-07-10 11:07:49 +0600

    showing view product and add to cart

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import productImage from '../../images/product.png'
+import { Card } from 'antd'
+import { EyeOutlined, ShoppingCartOutlined} from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+
+
+
+const { Meta } = Card
 
 const ProductCard = ({product}) => {
+
+    const { title, description, images, slug } = product
     return (
-        <div>
-            {JSON.stringify(product.title)}
-        </div>
+       
+        <Card cover={
+            <img src={images && images.length ? images[0].url : productImage } style={{height: "150px", objectFit: "cover"}} />
+        }
+        actions={[
+            <Link to={`/product/${slug}`}><EyeOutlined className="text-info" /> <br/> View Product</Link>, <><ShoppingCartOutlined className="text-warning"/> <br/> Add to cart</>
+        ]}
+        >
+            <Meta title={title} description={`${description && description.substring(0, 40)}...`} />
+        </Card>
+
     )
 }
 
