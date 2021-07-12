7a9c02123ac36143b640f4af1251298f2796403f
nickty <nickty.86@gmail.com>
2021-07-12 08:46:57 +0600

    category showing final

--- src/pages/category/CategoryHome.js
+++ src/pages/category/CategoryHome.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import ProductCard from '../../components/cards/ProductCard'
 import { getCategory } from '../../functions/category'
 
 const CategoryHome = ({match}) => {
@@ -12,12 +13,27 @@ const CategoryHome = ({match}) => {
         setLoading(true)
         getCategory(slug)
         .then(res => {
-            setCategory(res.data)
+            setCategory(res.data.category)
+            setProducts(res.data.products)
+            setLoading(false)
         })
     }, [])
     return (
-        <div>
-            <p>{JSON.stringify(category)}</p>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    {loading ? (<h4 className="text-center p-3 mt-5 mb-4 display-4 jumbotron">Loading...</h4>) : (
+                        <h4 className="text-center p-3 mt-5 mb-4 jumbotron">
+                            {products.length} Products in {category.name} Category
+                        </h4>
+                    ) }
+                </div>
+            </div>
+            <div className="row">
+                {products.map(p => (<div className="col" key={p._id}>
+                    <ProductCard product={p} />
+                    </div>))}
+            </div>
         </div>
     )
 }
