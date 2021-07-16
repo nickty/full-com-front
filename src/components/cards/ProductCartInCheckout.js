8b155b7c6411d422a245f4b7cf08ea3919f236f0
nickty <nickty.86@gmail.com>
2021-07-16 22:07:21 +0600

    showing image in the cart

--- src/components/cards/ProductCartInCheckout.js
+++ src/components/cards/ProductCartInCheckout.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import ModalImage from 'react-modal-image'
+import laptop from '../../images/product.png'
 
 const ProductCartInCheckout = ({p}) => {
     return (
         <tbody>
             <tr>
-                <td>image</td>
+                <td><div style={{width: "200px", height: 'auto'}}>
+                        {p.images.length ? (<ModalImage small={p.images[0].url} large={p.images[0].url} />) : (<ModalImage small={laptop} large={laptop}></ModalImage>) }
+                    </div></td>
                 <td>{p.title}</td>
                 <td>${p.price}</td>
                 <td>{p.brand}</td>
