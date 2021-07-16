70f4d186b4ebeca5392c7d2334388e21b75eeaa9
nickty <nickty.86@gmail.com>
2021-07-16 08:50:34 +0600

    save cart into redux state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
+import { cartReducer } from './cartReducer';
 import { searchReducer } from './searchReducer';
 
 import { userReducer } from './userReducer'
 
 const rootReducer = combineReducers({
     user: userReducer,
-    search: searchReducer
+    search: searchReducer,
+    cart: cartReducer
 })
 
 
