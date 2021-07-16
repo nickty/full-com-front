5ade80e53b3d41940ca79410722f485f605dcaad
nickty <nickty.86@gmail.com>
2021-07-16 08:31:23 +0600

    save cart into localstorage

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -4,6 +4,7 @@ import {Card } from 'antd'
 import { EyeOutlined, ShoppingCartOutlined} from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import { showAverage } from '../../functions/rating'
+import _ from 'lodash'
 
 
 
@@ -12,6 +13,29 @@ const { Meta } = Card
 const ProductCard = ({product}) => {
 
     const { title, description, price, images, slug } = product
+
+    const handleAddtocart = () => {
+        //create cart array
+        let cart = []
+        if(typeof window !== 'undefined'){
+            //if cart is in localStorage GET it
+            if(localStorage.getItem('cart')){
+                cart = JSON.parse(localStorage.getItem('cart'))
+            }
+            // push new product to cart 
+            cart.push({
+                ...product,
+                count: 1
+            })
+            //remove duplicates
+            let unique = _.unionWith(cart, _.isEqual)
+
+            //save to local strorage
+            // console.log('unique', unique)
+            localStorage.setItem('cart', JSON.stringify(unique))
+
+        }
+    }
     return (
        <>
        {product && product.ratings && product.ratings.length > 0 ? 
@@ -20,7 +44,7 @@ const ProductCard = ({product}) => {
             <img src={images && images.length ? images[0].url : productImage } style={{height: "150px", objectFit: "cover"}} />
         }
         actions={[
-            <Link to={`/product/${slug}`}><EyeOutlined className="text-info" /> <br/> View Product</Link>, <><ShoppingCartOutlined className="text-warning"/> <br/> Add to cart</>
+            <Link to={`/product/${slug}`}><EyeOutlined className="text-info" /> <br/> View Product</Link>, <a onClick={handleAddtocart}><ShoppingCartOutlined className="text-warning"/> <br/> Add to cart</a>
         ]}
         >
             <Meta title={`${title} $${price}`} description={`${description && description.substring(0, 40)}...`} />
