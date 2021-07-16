d4ea925b8bd7565e709ebab6b88b3f43f56daf2c
nickty <nickty.86@gmail.com>
2021-07-16 21:23:59 +0600

    get total with reduce function

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
 
@@ -7,10 +8,46 @@ const Cart = () => {
 
     const dispatch = useDispatch()
 
+    const getTotal= () => {
+        return cart.reduce((cv, nv) => {
+            return cv + nv.count * nv.price
+        }, 0)
+    }
+
     return (
         <div className="container-fluid">
-            <h4>Cart</h4>
-            {JSON.stringify(cart)}
+          
+            <div className="row">
+                <div className="col-md-8">
+                <h4>Cart / {cart.length}</h4>
+                    {!cart.length ? <h4>No product in cart.  <Link to="/shop">Contiue shoppong</Link></h4> : (
+                        "show cart items"
+                    )}
+                </div>
+                <div className="col-md-4">
+                        <h4>Order Summary</h4>
+                        <hr />
+                        <p>Products</p>
+                        {cart.map((c,i) => (
+                            <div key={i}>
+                                <p>{c.title} x {c.count} = ${c.price * c.count}</p>
+                            </div>
+                        ))}
+
+                        <hr />
+                        Total : <b>${getTotal()}</b>
+                        <hr />
+
+
+                        {
+                            user ? (
+                                <button className="btn btn-sm btn-primary mt-2">Procced to checkout</button>
+                            ) : (
+                                <button className="btn btn-sm btn-primary mt-2">Login to checkout</button>
+                            )
+                        }
+                </div>
+            </div>
         </div>
     )
 }
