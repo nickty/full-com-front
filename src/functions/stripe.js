308aac8c7f0109d855b7aec41c75036856c65241
nickty <nickty.86@gmail.com>
2021-07-18 08:15:50 +0600

    sending payment successful

--- src/functions/stripe.js
+++ src/functions/stripe.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const createPaymentIntent = async (authToken) =>{
-    return await axios.post(`${process.env.REACT_APP_API}/create-payment-intent`, {}, {
+export const createPaymentIntent = async (authToken, coupon) =>{
+    return await axios.post(`${process.env.REACT_APP_API}/create-payment-intent`, {couponApplied: coupon}, {
         headers: {
             authToken
         }
