9d6bf91394ded82bb57ce50eacb08c5301cb2989
nickty <nickty.86@gmail.com>
2021-07-18 07:25:45 +0600

    payment made succesfully

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
 import { useSelector, useDispatch } from 'react-redux'
 import { createPaymentIntent } from '../functions/stripe'
+import { Link } from 'react-router-dom'
 
 const StripeCheckout = ({history}) => {
 
@@ -10,7 +11,7 @@ const StripeCheckout = ({history}) => {
 
     const [succeeded, setSucceeded] = useState(false)
     const [error, setError] = useState(null)
-    const [processing, setProcessing] = useState('')
+    const [processing, setShowProcessing] = useState('')
     const [disabled, setDisabled] = useState(true)
     const [clientSecret, setClientSecret] = useState('')
 
@@ -20,16 +21,37 @@ const StripeCheckout = ({history}) => {
     useEffect(() => {
         createPaymentIntent(user.token).then(res => {
             console.log("Create payment intent", res.data.clientSecret)
-            setClientSecret(res.data)
+            setClientSecret(res.data.clientSecret)
         })
     }, [])
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setShowProcessing(true)
 
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement), 
+                billing_details: {
+                    name: e.target.name.value
+                }
+            }
+        })
+
+        if(payload.error){
+            setError(`Payment failed ${payload.error.message}`)
+            setShowProcessing(false)
+        } else {
+            console.log(JSON.stringify(payload, null, 4))
+            setError(null)
+            setShowProcessing(false)
+            setSucceeded(true)
+        }
     }
 
     const handleChange = (e) => {
-        
+        setDisabled(e.empty)
+        setError(e.error ? e.error.message : "")
     }
 
     const cardStyle = {
@@ -52,12 +74,15 @@ const StripeCheckout = ({history}) => {
 
     return (
         <div>
+            <p className={succeeded ? 'result-message' : 'result-message hidden'}>Payment Successful <Link to="/user/history">see i in your history</Link></p>
             <form id="payment-form" className="stripe-form" onSubmit={handleSubmit}>
                 <CardElement id="cart-element" options={cardStyle} onChange={handleChange} />
                 <button type="submit" className="stripe-button" disabled={processing || disabled || succeeded}>
                     {processing ? <div className="spinner" id="spinner"></div> : "Pay"}
                 </button>
+                {error && <div className="card-error">{error}</div>}
             </form>
+           
         </div>
     )
 }
