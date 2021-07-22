9a9ff391c205654498e1a9d7dc2ce9957dbc1af7
nickty <nickty.86@gmail.com>
2021-07-22 09:30:02 +0600

    showing order info

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -1,3 +1,4 @@
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons'
 import React, { useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import UserNav from '../../components/nav/UserNav'
@@ -22,9 +23,53 @@ const History = () => {
             setOrders(res.data)
         })
     
-    const showOrderInTable = order => (
-        <p>paymntinfo roder</p>
-    )
+    const showOrderInTable = order => 
+    <table className="table table-bordered">
+        <thead className="thead-light">
+        <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Price</th>
+            <th scope="col">Brand</th>
+            <th scope="col">Color</th>
+            <th scope="col">Count</th>
+            <th scope="col">Shipping</th>
+           
+        </tr>
+        </thead>
+
+        <tbody>
+            {order.products.map((p, i)=>(
+                <tr key={i}>
+                    <td>
+                        <b>
+                            {p.product.title}
+                        </b>
+                    </td>
+                    <td>
+                           {p.product.price}
+
+                    </td>
+                    <td>
+                           {p.product.brand}
+
+                    </td>
+                    <td>
+                           {p.color}
+
+                    </td>
+                    <td>
+                           {p.count}
+
+                    </td>
+                    <td>
+                           {p.product.shipping === 'yes' ? <CheckCircleOutlined style={{color: 'green'}} /> : <CloseCircleOutlined style={{color: 'red'}} />}
+
+                    </td>
+                    
+                </tr>
+            ))}
+        </tbody>
+    </table>
 
     const showEachOrders = () => orders.map((order, i) => (
         <div key={i} className="m-5 p-3 card">
