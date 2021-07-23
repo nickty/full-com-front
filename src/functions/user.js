60db211620b16e4a52805c779976999089772602
nickty <nickty.86@gmail.com>
2021-07-23 07:45:10 +0600

    wishlist function added

--- src/functions/user.js
+++ src/functions/user.js
@@ -41,3 +41,23 @@ export const getUserOrder = async (authtoken) => await axios.get(`${process.env.
         authtoken
     }
 })
+
+export const getWishlist = async (authtoken) => await axios.get(`${process.env.REACT_APP_API}/user/wishlist`, {
+    headers: {
+        authtoken
+    }
+})
+
+export const removeWishlist = async (productId, authtoken) => await axios.put(`${process.env.REACT_APP_API}/user/wishlist/${productId}`, 
+{productId}, 
+{
+    headers: {
+        authtoken
+    }
+})
+
+export const addToWishlist = async (productId, authtoken) => await axios.post(`${process.env.REACT_APP_API}/user/wishlist`, {
+    headers: {
+        authtoken
+    }
+})
